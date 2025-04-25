be9b8272753d6314d64d098ac9897adb2d227198
tuantq <kutuanonline199@gmail.com>
2025-04-25 20:19:00 +0700

    install rector-laravel

---
+++ rector.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use Rector\ValueObject\PhpVersion;
+use RectorLaravel\Rector\Class_\AnonymousMigrationsRector;
+use RectorLaravel\Rector\ClassMethod\AddGenericReturnTypeToRelationsRector;
+use RectorLaravel\Rector\Coalesce\ApplyDefaultInsteadOfNullCoalesceRector;
+use RectorLaravel\Rector\Expr\AppEnvironmentComparisonToParameterRector;
+use RectorLaravel\Rector\Expr\SubStrToStartsWithOrEndsWithStaticMethodCallRector\SubStrToStartsWithOrEndsWithStaticMethodCallRector;
+use RectorLaravel\Rector\FuncCall\NotFilledBlankFuncCallToBlankFilledFuncCallRector;
+use RectorLaravel\Rector\FuncCall\NowFuncWithStartOfDayMethodCallToTodayFuncRector;
+use RectorLaravel\Rector\FuncCall\RemoveDumpDataDeadCodeRector;
+use RectorLaravel\Rector\FuncCall\ThrowIfAndThrowUnlessExceptionsToUseClassStringRector;
+use RectorLaravel\Rector\FuncCall\TypeHintTappableCallRector;
+use RectorLaravel\Rector\If_\AbortIfRector;
+use RectorLaravel\Rector\If_\ReportIfRector;
+use RectorLaravel\Rector\If_\ThrowIfRector;
+use RectorLaravel\Rector\MethodCall\EloquentOrderByToLatestOrOldestRector;
+use RectorLaravel\Rector\MethodCall\EloquentWhereRelationTypeHintingParameterRector;
+use RectorLaravel\Rector\MethodCall\EloquentWhereTypeHintClosureParameterRector;
+use RectorLaravel\Rector\MethodCall\RedirectBackToBackHelperRector;
+use RectorLaravel\Rector\MethodCall\RedirectRouteToToRouteHelperRector;
+use RectorLaravel\Rector\MethodCall\ValidationRuleArrayStringValueToArrayRector;
+use RectorLaravel\Rector\MethodCall\WhereToWhereLikeRector;
+use RectorLaravel\Rector\PropertyFetch\OptionalToNullsafeOperatorRector;
+use RectorLaravel\Rector\StaticCall\CarbonSetTestNowToTravelToRector;
+use RectorLaravel\Rector\StaticCall\DispatchToHelperFunctionsRector;
+use RectorLaravel\Rector\StaticCall\EloquentMagicMethodToQueryBuilderRector;
+use RectorLaravel\Rector\StaticCall\MinutesToSecondsInCacheRector;
+use RectorLaravel\Rector\StaticCall\RequestStaticValidateToInjectRector;
+use RectorLaravel\Rector\StaticCall\RouteActionCallableRector;
+use RectorLaravel\Set\LaravelLevelSetList;
+use RectorLaravel\Set\LaravelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/bootstrap',
+        __DIR__.'/config',
+        __DIR__.'/public',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
+    ])
+    // uncomment to reach your current PHP version
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true
+    )
+    ->withSets([
+        SetList::DEAD_CODE,
+        LaravelLevelSetList::UP_TO_LARAVEL_120,
+        LaravelSetList::LARAVEL_LEGACY_FACTORIES_TO_CLASSES,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_COLLECTION,
+    ])
+    ->withRules([
+        AnonymousMigrationsRector::class,
+        EloquentMagicMethodToQueryBuilderRector::class,
+        EloquentOrderByToLatestOrOldestRector::class,
+        AbortIfRector::class,
+        ReportIfRector::class,
+        ThrowIfRector::class,
+        RedirectRouteToToRouteHelperRector::class,
+        EloquentWhereRelationTypeHintingParameterRector::class,
+        EloquentWhereTypeHintClosureParameterRector::class,
+        ValidationRuleArrayStringValueToArrayRector::class,
+        RedirectBackToBackHelperRector::class,
+        WhereToWhereLikeRector::class,
+        NowFuncWithStartOfDayMethodCallToTodayFuncRector::class,
+        ThrowIfAndThrowUnlessExceptionsToUseClassStringRector::class,
+        TypeHintTappableCallRector::class,
+        NotFilledBlankFuncCallToBlankFilledFuncCallRector::class,
+        RemoveDumpDataDeadCodeRector::class,
+        CarbonSetTestNowToTravelToRector::class,
+        MinutesToSecondsInCacheRector::class,
+        DispatchToHelperFunctionsRector::class,
+        RequestStaticValidateToInjectRector::class,
+        RouteActionCallableRector::class,
+        OptionalToNullsafeOperatorRector::class,
+        SubStrToStartsWithOrEndsWithStaticMethodCallRector::class,
+        AppEnvironmentComparisonToParameterRector::class,
+        ApplyDefaultInsteadOfNullCoalesceRector::class,
+        AddGenericReturnTypeToRelationsRector::class,
+    ])
+    ->withImportNames(removeUnusedImports: true)
+    ->withPhpVersion(PhpVersion::PHP_82);
